8dbababa63361f6169ae93b9ea3ab0586c433ebe
0aix <brianx2000@gmail.com>
2020-04-29 03:11:50 -0700

    stats

--- StrikingDummy/main.cpp
+++ StrikingDummy/main.cpp
@@ -9,20 +9,20 @@ int main()
 	StrikingDummy::Stats stats;
 
 	// samurai
-	stats.weapon_damage = 122;
-	stats.main_stat = 4441; // 4230
-	stats.pot_stat = stats.main_stat + 312;
-	stats.critical_hit = 3124;
-	stats.direct_hit = 2938;
-	stats.determination = 1893;
-	stats.skill_speed = 814;
-	stats.auto_attack = 107.36;
+	stats.weapon_damage = 128;
+	stats.main_stat = 5101;
+	stats.pot_stat = stats.main_stat + 398;
+	stats.critical_hit = 3466;
+	stats.direct_hit = 1429;
+	stats.determination = 1030;
+	stats.skill_speed = 3253;
+	stats.auto_attack = 112.64;
 	stats.auto_delay = 2.64;
 
 	StrikingDummy::Samurai sam(stats);
 	StrikingDummy::TrainingDummy dummy(sam);
 	StrikingDummy::StrikingDummy practice(sam);
-	//dummy.train();
-	dummy.trace();
+	dummy.train();
+	//dummy.trace();
 	//practice.start();
 }
