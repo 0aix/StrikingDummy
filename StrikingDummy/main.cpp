d2f9697f6597061d20275934f4f19a3233303fed
0aix <brianx2000@gmail.com>
2021-12-18 21:30:25 -0500

    add pre f3 in main

--- StrikingDummy/main.cpp
+++ StrikingDummy/main.cpp
@@ -13,12 +13,13 @@ StrikingDummy::Stats relic_crit(180, 5884, 464, 4142, 2065, 1635, 1929);
 
 int main()
 {
-	StrikingDummy::BlackMage blm(relic_crit, StrikingDummy::BlackMage::Opener::PRE_T3, StrikingDummy::BlackMage::ActionSet::FULL);
+	//StrikingDummy::BlackMage blm(relic_crit, StrikingDummy::BlackMage::Opener::PRE_T3, StrikingDummy::BlackMage::ActionSet::FULL);
+	StrikingDummy::BlackMage blm(relic_crit, StrikingDummy::BlackMage::Opener::PRE_F3, StrikingDummy::BlackMage::ActionSet::FULL);
 	//StrikingDummy::BlackMage blm(relic_crit, StrikingDummy::BlackMage::Opener::PRE_B3, StrikingDummy::BlackMage::ActionSet::STANDARD);
 	StrikingDummy::TrainingDummy dummy(blm);
 	StrikingDummy::StrikingDummy practice(blm);
-	dummy.train();
-	//dummy.trace();
+	//dummy.train();
+	dummy.trace();
 	//dummy.metrics();
 	//dummy.dist(450, 10000);
 	//dummy.study(0);
